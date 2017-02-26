6ebc7230049e2a63ce9d701b8531f9c39bc41786
shivam04 <sinhahsivam04@gmail.com>
2017-02-26 18:03:08 +0530

    decision trees

--- ud120-projects/decision_tree/Speeding-Up-Via-Feature-Selection-1.py
+++ ud120-projects/decision_tree/Speeding-Up-Via-Feature-Selection-1.py
@@ -19,15 +19,15 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+print len(features_train[0])
 
+# from sklearn import tree
+# clf = tree.DecisionTreeClassifier(min_samples_split=40)
+# clf.fit(features_train,labels_train)
+# pred = clf.predict(features_test)
+# from sklearn.metrics import accuracy_score
+# acc = accuracy_score(pred,labels_test)
 
-from sklearn import tree
-clf = tree.DecisionTreeClassifier(min_samples_split=40)
-clf.fit(features_train,labels_train)
-pred = clf.predict(features_test)
-from sklearn.metrics import accuracy_score
-acc = accuracy_score(pred,labels_test)
-
-print acc
+# print acc
 
 
